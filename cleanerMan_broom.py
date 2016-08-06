d6c94136c6fefdafcc36c403ea17dc8f7737f0d2
niceprogram <niceprogram@yahoo.com>
2016-08-07 01:09:39 +0200

    Update cleanerMan_broom.py

--- cleanerMan_broom.py
+++ cleanerMan_broom.py
@@ -24,6 +24,16 @@ new_grid_no = None
 read_grid = None
 Maze_field = None
 
+# the broom forms an O around the CleanerMan. It will sweep away all the LEDs, except the walls.
+def the_broom():
+  global broom_x, broom_y, broom_grid_no, new_grid_no, read_grid, Maze_field, Mx, My, grid_move_ok
+  for broom_x in range(-1, 2):
+    for broom_y in range(-8, 9, 8):
+      broom_grid_no = new_grid_no + (broom_x + broom_y)
+      read_grid = Maze_field[int((new_grid_no + 1) - 1)]
+      if ((0 < Mx + broom_x and Mx + broom_x < 7) and (0 < My + broom_y and My + broom_y < 7)) and grid_move_ok == read_grid:
+        sense.set_pixel((Mx + broom_x),(My + broom_y),[0,0,0])   # (x,y,r,g,b)
+
 # Move CleanerMan within the confides of the maze with the arrow keys.
 # The maze is read to see if the CleanerMan can move to the next position
 def move_M():
@@ -32,25 +42,12 @@ def move_M():
   Mnew_x = Mx + newXY[0]
   Mnew_y = My + newXY[1]
   new_grid_no = Mnew_x + Mnew_y * 8
-  read_grid = Maze_field[int(new_grid_no)]
+  read_grid = Maze_field[int((new_grid_no + 1) - 1)]
   if grid_move_ok == read_grid:
-    sense.set_pixel(Mx,My,[55,55,0]) 
     Mx = Mnew_x
     My = Mnew_y
-    the_broom()
     sense.set_pixel(Mx,My,[255,255,0])   # (x,y,r,g,b)
-    
-
-# the broom forms an O around the CleanerMan. It will sweep away all the LEDs, except the walls.
-def the_broom():
-  global new_grid_no, broom_x, broom_y, broom_grid_no, read_grid, grid_move_ok, Mx, Mnew_x, My, Mnew_y
-  for broom_x in range(-1, 2):
-    for broom_y in range(-8, 9, 8):
-      broom_grid_no =  new_grid_no + (broom_x + broom_y)
-      read_grid = Maze_field[int(broom_grid_no)]
-      print(broom_grid_no)
-      if grid_move_ok == read_grid:
-        sense.set_pixel(Mx+broom_x,My+(broom_y/8),[0,0,0])   # (x,y,r,g,b)
+    the_broom()
 
 # listen to the key input, return X&Y for the CleanerMan
 def listen_for_key():
@@ -58,11 +55,11 @@ def listen_for_key():
   key = screen.getch()
   tempX = 0
   tempY = 0
-  if Mx != 0 and key == curses.KEY_LEFT:
+  if Mx != 0 and key == curses.KEY_UP:
     tempX = -1
   elif Mx != 7 and key == curses.KEY_RIGHT:
     tempX = 1
-  elif My != 0 and key == curses.KEY_UP:
+  elif My != 0 and key == curses.KEY_LEFT:
     tempY = -1
   elif My != 7 and key == curses.KEY_DOWN:
     tempY = 1
